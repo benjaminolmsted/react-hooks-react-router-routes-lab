afca98c28920f2c77089c57b69ff46270ce690f8
benjamin <benjamin.olmsted@gmail.com>
2021-07-30 15:55:20 -0700

    commit tree 745dd43c64429d6df9be9749b7b12efa518814f4

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const renderedActors = actors.map(actor=>{
+    return (
+      <div key={actor.name}>
+        <h2>{actor.name}</h2>
+        <ul>{actor.movies.map(movie=><li key={movie}>{movie}</li>)}</ul>
+      </div>
+    )
+  })
+  return <div>
+            <h1>Actors Page</h1>
+              {renderedActors}</div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+ const renderedMovies = movies.map(movie => {
+    return ( <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <h3>runtime {movie.time} minutes</h3>
+      <ul>
+        {movie.genres.map(genre=><li key={genre}>{genre}</li>)}
+      </ul>
+    </div>)
+  })
+
+  return <div>
+          <h1>Movies Page</h1>
+            {renderedMovies}
+         </div>;
 }
 
 export default Movies;
